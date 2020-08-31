d0cf004da341d08a4517fe2e7d6186fd9f4e4097
0lxb <luoxiaobing0926@gmail.com>
2020-08-31 10:28:46 +0800

    [CHG]update cscope.sh script

--- cscope.sh
+++ cscope.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" > cscope.files
+find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.lds" -o -name "*.go" -o -name "*.s" -o -name "*.S" -o -name "*.mk"> cscope.files
 cscope -bkq -i cscope.files
 ctags -R
